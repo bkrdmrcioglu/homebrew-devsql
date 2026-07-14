98ea4270a61f37e87faf0998cff43fd086c45694
Bekir Demircioglu <bkrdmrcioglu@gmail.com>
2026-07-14 11:09:39 +0300

    Set DevSQL 0.1.0 notarized sha256.

--- Casks/devsql.rb
+++ Casks/devsql.rb
@@ -1,6 +1,6 @@
 cask "devsql" do
   version "0.1.0"
-  sha256 "REPLACE_AFTER_NOTARIZED_ZIP"
+  sha256 "d4b1d64c203d28cdac6e9ad8410ac29cae32f2ebfe47e7578a023adb38c575fc"
 
   url "https://github.com/bkrdmrcioglu/devsql-site/releases/download/v#{version}/DevSQL-#{version}.zip"
   name "DevSQL"
